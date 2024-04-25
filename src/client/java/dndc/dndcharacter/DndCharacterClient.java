1913240f47c78724da17cbf1d878a7f14347ffca
Peanut-Gallery66 <niffirg330@gmail.com>
2024-04-25 14:30:55 -0500

    Uploaded Starter Files

---
+++ src/client/java/dndc/dndcharacter/DndCharacterClient.java
@@ -0,0 +1,10 @@
+package dndc.dndcharacter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DndCharacterClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
